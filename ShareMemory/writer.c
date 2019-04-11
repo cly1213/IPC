ec3c1d9efe070b444b771a9252d5b21130972358
Parallels <parallels@debian-gnu-linux-vm.localdomain>
2019-04-11 14:48:19 +0800

    update3

---
+++ ShareMemory/writer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+extern int create_and_write_shared_memory (char *mmap_key, 
+		                           char *value, 
+                                           unsigned int size);
+
+int main (int argc, char **argv){
+
+    char *key = "/introduction";
+    char *intro = "Hello World, Using share memory 3+";
+    create_and_write_shared_memory(key, intro, strlen(intro));
+    return 0;
+}
